d2d86dd886203b36cd06ab8e7920b9b97180848f
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2025-04-01 16:00:29 -0300

    first commit - created project

---
+++ src/main/java/application/WorkshopSpringboot3Mongodb.java
@@ -0,0 +1,14 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot3Mongodb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot3Mongodb.class, args);
+	}
+
+}
+
